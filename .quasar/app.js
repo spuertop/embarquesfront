177786be1885761cdeb5dc0a1214cbb358eb6db8
SP <sergipuerto@gmail.com>
2023-01-10 23:35:58 +0100

    last

--- .quasar/app.js
+++ .quasar/app.js
@@ -38,6 +38,8 @@ export default async function (createAppFn, quasarUserOptions) {
   const app = createAppFn(RootComponent)
 
   
+  app.config.performance = true
+  
 
   app.use(Quasar, quasarUserOptions)
 
